3147c14e5b7d31af9cc3f1ab3772d6bfd65ccb7c
Ryan Casey <ryepdx@gmail.com>
2014-09-26 17:32:33 -0700

    Define our interface.

---
+++ funcs.h
@@ -0,0 +1,7 @@
+int is_hex(char arg[]);
+int get_word(char buffer[], char hex_chunk[], char *wordlist[]);
+int is_hex_padded(char hex_string[]);
+int normalized_hex_string_length(char hex_string[]);
+int normalize_hex_string(char buffer[], char hex_string[]);
+int max_phrase_length(char hex_string[]);
+int get_phrase(char buffer[], char hex_string[], char *wordlist[]);
