84fd74b96f430860c2702ebd2da5116998db420c
Ryan Casey <ryepdx@gmail.com>
2014-09-27 00:51:31 -0700

    Newline after program complains about missing words.txt file.

--- keyphrase.c
+++ keyphrase.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
     FILE *fp = fopen(WORDFILE, "r");
 
     if (!fp) {
-       printf("Could not find wordlist: '%s' is missing!", WORDFILE); 
+       printf("Could not find wordlist: '%s' is missing!\n", WORDFILE); 
        return 1;
     }
 
