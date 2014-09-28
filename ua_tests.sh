5fb374c4aec2d26e654caac904a789b32f4338ce
Ryan Casey <ryepdx@gmail.com>
2014-09-28 15:45:48 -0700

    Remove superfluous lines.

--- ua_tests.sh
+++ ua_tests.sh
@@ -65,7 +65,6 @@ if [ "$1" == "--watch" ] || [ "$1" == "-w" ]; then
         clear
         run_tests  
         change=$(inotifywait -q -e close_write,moved_to,create {*.c,*.h})
-        change=${change#./ * }
     done
 else
     run_tests
