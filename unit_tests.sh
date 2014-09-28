cf2767f8ccaaa05fbe824f338e756c752955755c
Ryan Casey <ryepdx@gmail.com>
2014-09-28 15:50:23 -0700

    Remove superfluous variable.

--- unit_tests.sh
+++ unit_tests.sh
@@ -8,7 +8,7 @@ if [ "$1" == "--watch" ] || [ "$1" == "-w" ]; then
     while true; do
         clear
         run_tests
-        change=$(inotifywait -q -e close_write,moved_to,create {*.c,*.h})
+        inotifywait -q -e close_write,moved_to,create {*.c,*.h}
     done
 else
     run_tests
